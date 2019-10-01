2360b014f666d7a1a98e259630b921b6ccf73a9f
Kevin Damian Ta <kevindamianta@gmail.com>
2019-10-01 10:57:12 -0700

    Script to insert data
    Script to insert data into article table for blogful

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,24 @@
+INSERT INTO articles 
+    (title, content, date_published) 
+VALUES
+('title1', 'content-1', '2018-12-31'),
+('title2',	'content-2', '2019-01-02'),
+('title3',	'content-3', '2019-01-03'),
+('title1',	'content-1', '2019-01-01'),
+('title4',	'content-4', '2019-01-04'),
+('title5',	'content-5', '2019-01-05'),
+('title6',	'content-6', '2019-01-06'),
+('title7',	'content-7', '2019-01-07'),
+('title8',	'content-8', '2019-01-08'),
+('title9',	'content-9', '2019-01-09'),
+('title10',	'content-10', '2019-01-10'),
+('title11',	'content-11', '2019-01-11'),
+('title12',	'content-12', '2019-01-12'),
+('title13',	'content-13', '2019-01-13'),
+('title14',	'content-14', '2019-01-14'),
+('title15',	'content-15', '2019-01-15'),
+('title16',	'content-16', '2019-01-16'),
+('title17',	'content-17', '2019-01-17'),
+('title18',	'content-18', '2019-01-18'),
+('title19',	'content-19', '2019-01-19'),
+('title20',	'content-20', '2019-01-20')
